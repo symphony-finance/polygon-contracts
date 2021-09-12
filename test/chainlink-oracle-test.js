9df4f4f20ced6001047ae817bfe768ffdd7e4281
justAvariable <symphonyfi.dev@gmail.com>
2021-09-12 19:55:21 +0530

    tests: extreme values tests

--- test/chainlink-oracle-test.js
+++ test/chainlink-oracle-test.js
@@ -1,10 +1,11 @@
 const hre = require("hardhat");
 const { expect } = require("chai");
+const { BigNumber: EthersBN } = require("ethers");
 const { default: BigNumber } = require("bignumber.js");
-const { ZERO_ADDRESS } = require("@openzeppelin/test-helpers/src/constants");
 const ChainlinkArtifacts = require(
     "../artifacts/contracts/oracles/ChainlinkOracle.sol/ChainlinkOracle.json"
 );
+const { ZERO_ADDRESS } = require("@openzeppelin/test-helpers/src/constants");
 
 const daiAddress = "0x6b175474e89094c44da98b954eedeac495271d0f";
 const usdcAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
@@ -175,11 +176,98 @@ describe("Chainlink Oracle Test", () => {
         );
     });
 
-    // TODO: Complete this test
     it("should work for small value", async () => {
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: ["0xAb7677859331f95F25A3e7799176f7239feb5C44"]
+        });
+
+        const deployer = await ethers.provider.getSigner(
+            "0xAb7677859331f95F25A3e7799176f7239feb5C44"
+        );
+        deployer.address = deployer._address;
+
+        // Deploy Chainlink Oracle
+        const ChainlinkOracle = await hre.ethers.getContractFactory("ChainlinkOracle");
+        let chainlinkOracle = await ChainlinkOracle.deploy(deployer.address);
+
+        await chainlinkOracle.deployed();
+
+        chainlinkOracle = new ethers.Contract(
+            chainlinkOracle.address,
+            ChainlinkArtifacts.abi,
+            deployer
+        );
+
+        await chainlinkOracle.updateTokenFeed(daiAddress, daiFeed);
+        await chainlinkOracle.updateTokenFeed(aaveAddress, aaveFeed);
+
+        await chainlinkOracle.updatePriceSlippage(0);
+        
+        // DAI to AAVE price
+        const inputAmount = 1;
+
+        let expectedOutputAmount = 0;
+
+        let result = await chainlinkOracle.get(
+            daiAddress, aaveAddress, inputAmount
+        );
+        expect(Number(result.amountOutWithSlippage)).to.eq(
+            Number(expectedOutputAmount)
+        );
+
+        // AAVE to DAI price
+        result = await chainlinkOracle.get(
+            aaveAddress, daiAddress, inputAmount
+        );
+
+        expectedOutputAmount = 213; // aave price on 28/6/21 8:39 UTC
+        expect(Number(result.amountOut)).to.eq(expectedOutputAmount);
     });
 
-    // TODO: Complete this test
-    it("should work for large value", async () => {
+    it("should work for large value (USD denomination)", async () => {
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: ["0xAb7677859331f95F25A3e7799176f7239feb5C44"]
+        });
+
+        const deployer = await ethers.provider.getSigner(
+            "0xAb7677859331f95F25A3e7799176f7239feb5C44"
+        );
+        deployer.address = deployer._address;
+
+        // Deploy Chainlink Oracle
+        const ChainlinkOracle = await hre.ethers.getContractFactory("ChainlinkOracle");
+        let chainlinkOracle = await ChainlinkOracle.deploy(deployer.address);
+
+        await chainlinkOracle.deployed();
+
+        chainlinkOracle = new ethers.Contract(
+            chainlinkOracle.address,
+            ChainlinkArtifacts.abi,
+            deployer
+        );
+
+        const daiUSDFeed = "0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9";
+        const aaveUSDFeed = "0x547a514d5e3769680Ce22B2361c10Ea13619e8a9";
+        await chainlinkOracle.updateTokenFeed(daiAddress, daiUSDFeed);
+        await chainlinkOracle.updateTokenFeed(aaveAddress, aaveUSDFeed);
+        await chainlinkOracle.updatePriceSlippage(0);
+
+        // DAI to AAVE price
+        const inputAmount = EthersBN.from(100).mul(
+            EthersBN.from(10).pow(EthersBN.from(30))
+        ).toString();
+
+        const outputAmount = EthersBN.from(469).mul(
+            EthersBN.from(10).pow(EthersBN.from(27))
+        ).toString();
+
+        let result = await chainlinkOracle.get(
+            daiAddress, aaveAddress, inputAmount
+        );
+        
+        expect(Number(result.amountOutWithSlippage)).to.be
+            .greaterThan(Number(outputAmount));
     });
 });
