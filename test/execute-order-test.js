54c0c044749c9c5f54baf51f70d61052a81f3039
Kakashi <symphfi@gmail.com>
2021-09-12 12:54:04 +0530

    added test for execute using balancer

--- test/execute-order-test.js
+++ test/execute-order-test.js
@@ -20,6 +20,10 @@ const { ZERO_ADDRESS, ZERO_BYTES32 } = require("@openzeppelin/test-helpers/src/c
 
 const daiAddress = "0x6b175474e89094c44da98b954eedeac495271d0f";
 const usdcAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+const balAddress = "0xba100000625a3754423978a60c9317c58a424e3D";
+
+const daiBalPool = "0x148ce9b50be946a96e94a4f5479b771bab9b1c59000100000000000000000054";
+const usdcBalPool = "0x9c08c7a7a89cfd671c79eacdc6f07c1996277ed5000200000000000000000025";
 
 const inputAmount = new BigNumber(10).times(
     new BigNumber(10).exponentiatedBy(new BigNumber(18))
@@ -194,8 +198,153 @@ describe("Execute Order Test", () => {
         );
     });
 
-    // TODO: Complete this test
     it("Should execute order with Balancer Handler & Aave Yield", async () => {
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: ["0xAb7677859331f95F25A3e7799176f7239feb5C44"]
+        });
+
+        const deployer = await ethers.provider.getSigner(
+            "0xAb7677859331f95F25A3e7799176f7239feb5C44"
+        );
+        deployer.address = deployer._address;
+
+        // Create USDC contract instance
+        const usdcContract = new ethers.Contract(
+            usdcAddress,
+            IERC20Artifacts.abi,
+            deployer
+        );
+
+        // Create DAI contract instance
+        const daiContract = new ethers.Contract(
+            daiAddress,
+            IERC20Artifacts.abi,
+            deployer
+        );
+
+        // Deploy Chainlink Oracle
+        const ChainlinkOracle = await hre.ethers.getContractFactory("ChainlinkOracle");
+        let chainlinkOracle = await ChainlinkOracle.deploy(deployer.address);
+
+        await chainlinkOracle.deployed();
+
+        chainlinkOracle = new ethers.Contract(
+            chainlinkOracle.address,
+            ChainlinkArtifacts.abi,
+            deployer
+        );
+        await chainlinkOracle.updateTokenFeed(
+            usdcAddress,
+            "0x986b5E1e1755e3C2440e960477f25201B0a8bbD4", // USDC-ETH
+        );
+
+        await chainlinkOracle.updateTokenFeed(
+            daiAddress,
+            "0x773616E4d11A78F511299002da57A0a94577F1f4", // DAI-ETH
+        );
+
+        await chainlinkOracle.updatePriceSlippage(80);
+
+        // Deploy Symphony Contract
+        const Symphony = await ethers.getContractFactory("Symphony");
+
+        let symphony = await upgrades.deployProxy(
+            Symphony,
+            [
+                deployer.address,
+                deployer.address,
+                40,
+                chainlinkOracle.address,
+            ]
+        );
+
+        await symphony.deployed();
+
+        symphony = new ethers.Contract(
+            symphony.address,
+            SymphonyArtifacts.abi,
+            deployer
+        );
+
+        // Deploy AaveYield Contract
+        const AaveYield = await hre.ethers.getContractFactory("AaveYield");
+
+        const configParams = config.mainnet;
+        let aaveYield = await upgrades.deployProxy(
+            AaveYield,
+            [
+                symphony.address,
+                deployer.address,
+                configParams.aaveLendingPool,
+                configParams.aaveProtocolDataProvider,
+                configParams.aaveIncentivesController
+            ]
+        );
+
+        await aaveYield.deployed();
+
+        aaveYield = new ethers.Contract(
+            aaveYield.address,
+            AaveYieldArtifacts.abi,
+            deployer
+        );
+
+        await symphony.updateStrategy(daiAddress, aaveYield.address);
+        await symphony.updateBufferPercentage(daiAddress, 4000);
+
+        // Deploy Sushiswap Handler
+        const BalancerHandler = await ethers.getContractFactory("BalancerHandler");
+
+        const balancerHandler = await BalancerHandler.deploy(
+            configParams.balancerVault,
+            symphony.address
+        );
+
+        await balancerHandler.deployed();
+
+        // Add Handler
+        await symphony.addHandler(balancerHandler.address);
+
+        await daiContract.approve(symphony.address, approveAmount);
+        await usdcContract.approve(symphony.address, approveAmount);
+
+        await symphony.addWhitelistAsset(daiAddress);
+
+        // Create Order
+        const tx = await symphony.createOrder(
+            deployer.address,
+            daiAddress,
+            usdcAddress,
+            inputAmount,
+            minReturnAmount,
+            stoplossAmount
+        );
+
+        const receipt = await tx.wait();
+        const events = receipt.events.filter((x) => { return x.event == "OrderCreated" });
+
+        const orderId = events[0].args[0];
+        const orderData = events[0].args[1];
+
+        const usdcBalBeforeExecute = await usdcContract.balanceOf(deployer.address);
+
+        // Advancing 100 blocks
+        for (let i = 0; i < 100; ++i) {
+            await time.advanceBlock();
+        };
+
+        const intermidiateAmount = "0";
+        const data = encodeHandlerData(balAddress, intermidiateAmount, daiBalPool, usdcBalPool);
+
+        // Execute Order
+        await symphony.executeOrder(orderId, orderData, balancerHandler.address, data)
+
+        const usdcBalAfterExecute = await usdcContract.balanceOf(deployer.address);
+
+        expect(Number(usdcBalAfterExecute)).to.be.greaterThanOrEqual(
+            Number(usdcBalBeforeExecute) + Number(expectedReturn)
+        );
     });
 
     it("Should execute existing order if strategy removed", async () => {
@@ -376,5 +525,15 @@ const encodeData = (router, slippage, codeHash, path) => {
     return abiCoder.encode(
         ['address', 'uint256', 'bytes32', 'address[]'],
         [router, slippage, codeHash, path]
+    )
+}
+
+
+const encodeHandlerData = (intermidiate, intermidiateAmount, poolA, poolB) => {
+    const abiCoder = new AbiCoder();
+
+    return abiCoder.encode(
+        ['address', 'uint256', 'bytes32', 'bytes32'],
+        [intermidiate, intermidiateAmount, poolA, poolB]
     )
 }
