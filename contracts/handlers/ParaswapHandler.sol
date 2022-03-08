8d81138a93c4e47a12ce785ba0abe31cf88083bc
Kakashi <symphfi@gmail.com>
2022-03-08 18:18:26 +0530

    fix unused import

--- contracts/handlers/ParaswapHandler.sol
+++ contracts/handlers/ParaswapHandler.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "../interfaces/IHandler.sol";
-import "../libraries/paraswap/Utils.sol";
 
 /// @notice Paraswap Handler used to execute an order
 contract ParaswapHandler is IHandler {
